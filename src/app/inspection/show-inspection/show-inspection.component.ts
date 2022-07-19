7ff6e9147f7f9beb54bccf82a38b6c84e7018f76
m-komilov <muhammadabdulloh.uzinfocom@gmail.com>
2022-07-19 17:53:26 +0500

    update

--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-show-inspection',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowInspectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: InspectionApiService) { }
 
   ngOnInit(): void {
   }
